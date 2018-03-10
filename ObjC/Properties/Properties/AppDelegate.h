8e68bceb01dafba4f552f5a97f7b942a609bf04b
Artem Lytkin <lytkinreal@mail.ru>
2018-03-10 11:17:55 +0300

    Properties part 1 start

---
+++ ObjC/Properties/Properties/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Properties
+//
+//  Created by macbook air on 10/03/2018.
+//  Copyright © 2018 Lytkin Artem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
