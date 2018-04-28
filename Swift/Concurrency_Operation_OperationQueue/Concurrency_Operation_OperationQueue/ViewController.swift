6158471ad8e905f58d3224b9350c0f04be952b34
Artem Lytkin <lytkinreal@mail.ru>
2018-04-28 06:18:53 +0300

    Concurrency with Operation and OperationQueue is started

---
+++ Swift/Concurrency_Operation_OperationQueue/Concurrency_Operation_OperationQueue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concurrency_Operation_OperationQueue
+//
+//  Created by macbook air on 28/04/2018.
+//  Copyright © 2018 Lytkin Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
